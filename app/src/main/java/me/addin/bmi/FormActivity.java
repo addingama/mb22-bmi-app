86a4769eb9228ae6c8bec50cce6ee95bfa6204ba
Addin Gama Bertaqwa <addingama@gmail.com>
2022-04-20 10:15:55 +0800

    FormActivity finished

--- app/src/main/java/me/addin/bmi/FormActivity.java
+++ app/src/main/java/me/addin/bmi/FormActivity.java
@@ -2,7 +2,10 @@ package me.addin.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Button btnHitung = (Button) findViewById(R.id.btn_hitung);
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Make this dynamic
+                double bb = 50; // kg
+                double tb = 1.68; // m
+                double bmi = bb/(Math.pow(tb, 2));
+
+                Intent intent = new Intent(FormActivity.this, ResultActivity.class);
+                intent.putExtra("bmi", bmi);
+                startActivity(intent);
+            }
+        });
     }
 }
