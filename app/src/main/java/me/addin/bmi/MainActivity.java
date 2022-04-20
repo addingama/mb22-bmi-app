1e5cebd311f46ec183990c5a1b61136a911bf603
Addin Gama Bertaqwa <addingama@gmail.com>
2022-04-20 10:09:55 +0800

    Open FormActivity

--- app/src/main/java/me/addin/bmi/MainActivity.java
+++ app/src/main/java/me/addin/bmi/MainActivity.java
@@ -2,7 +2,12 @@ package me.addin.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvNama = (TextView) findViewById(R.id.tv_nama);
+        Button btnLanjut = (Button) findViewById(R.id.btn_lanjut);
+
+        btnLanjut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Make this dynamic
+                String nama = "test";
+                if (nama.equals("")) {
+                    Toast.makeText(MainActivity.this, "Nama tidak boleh kosong", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                    intent.putExtra("nama", nama);
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+
 }
